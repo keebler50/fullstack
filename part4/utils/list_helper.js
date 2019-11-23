624a75627df931ac4b2c17bcfb454e8391a9ca29
Ken W <kwbuechel@gmail.com>
2019-11-23 12:08:25 -0500

    exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,6 +3,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+  return blogs.map(b => {
+    if (b.likes && typeof b.likes === 'number') return b.likes
+    return 0
+  }).reduce((acc, curr) => acc + curr)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
