e32cdad0d11749841fa822cc27a0e6a638e78289
Ken W <kwbuechel@gmail.com>
2019-12-05 19:54:36 -0500

    exercise 4.13

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,17 +2,8 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response, next) => {
-  const body = request.body
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
-  const savedBlog = await blog.save()
-  response.json(savedBlog.toJSON())
+  const blogs = await Blog.find({})
+  response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response, next) => {
